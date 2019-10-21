ca84072e7f9fac39205a541be549517524fb581a
tanja84.08 <tanja84.08@gmail.com>
2019-10-21 21:10:01 +0300

    is complete

---
+++ Module3_7/Module3_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class ArrayExample
+{
+    static void Main()
+    {
+        Console.WriteLine("введите количество элементов массива");
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            
+            if (a[i] < a[i + 1])
+            {
+                Console.WriteLine("число массива,больше предыдущего= {1}",i+1, a[i + 1]);
+            }
+
+        }
+        Console.ReadKey();
+    }
+
+}
