b1e11e14e2c1e4cfeae3e81b7dbfa0b0de5f6379
tanja84.08 <tanja84.08@gmail.com>
2019-10-21 20:57:17 +0300

    is complete

---
+++ Module3_4/Module3_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число, которое нужно записать наоборот");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write(n % 10);
+            while ((n /= 10) != 0)
+            Console.Write(n % 10);
+            Console.ReadKey();
+        }
+   
+    }
+}
