d34836d632a42b17d7e55328cf19193f1f9b11fe
tanja84.08 <tanja84.08@gmail.com>
2019-10-24 20:50:28 +0300

    done

---
+++ Module3_8/Module3_8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace cyber1
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.Write(" n = ");
+            int N = int.Parse(Console.ReadLine());//границы снизу и сверху
+            int[,] MM = new int[N, N];
+            int row = 0, col = 0, dx = 0, dy = 1, dirChanges = 0, gran = N;
+
+            for (int i = 0; i < MM.Length; i++)
+            {
+                MM[row, col] = i + 1;
+
+                if (--gran == 0)
+                {
+                    gran = N * (dirChanges % 2) + N * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+                    int temp = dy;
+                    dy = -dx;
+                    dx = temp;
+                    dirChanges++;
+                }
+
+                col += dy;
+                row += dx;
+            }
+
+            for (int i = 0; i < MM.GetLength(0); i++)
+            {
+                for (int j = 0; j < MM.GetLength(0); j++)
+                {
+                    Console.Write(MM[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
